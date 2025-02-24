0c7feea9a008ad9f097693fb170c1b6b2f242776
TienBigD <nguyentrongtien30@gmail.com>
2025-02-24 15:45:56 +0700

    aaaaaaa

--- js/orderFood.js
+++ js/orderFood.js
@@ -5,13 +5,21 @@ async function showFoods() {
 
     data.forEach(element => {
         foods.innerHTML += `
-        <div class="card">
-                                    <span>1</span>
-                                    <img src="https://cdn-icons-png.flaticon.com/512/2251/2251855.png"
-                                        class="card-img-top w-50 mx-auto" alt="...">
-                                    <div class="card-body">
-
-                                    </div>
+        <div class="col">
+        <div class="card p-2">
+                                <div class="card_header">
+                                    <span>${element.id}</span>
+                                    <h4>${element.nameFood}</h4>
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                </div>
+                                <img src="${element.img}" class="card-img-top w-50 mx-auto" alt="...">
+                                <h5 class="text-center text-danger">$${element.price}</h5>
+                                <div class="card-body">
+                                    <i class="fa-solid fa-circle-minus"></i>
+                                    <input type="text" value="0" class="quantity">
+                                    <i class="fa-solid fa-circle-plus"></i>
+                                </div>
+                            </div>
                                 </div>`
     });
 
